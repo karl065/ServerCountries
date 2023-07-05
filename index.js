12a25568dcacf3354fe11c5b4b9ca62c610818bd
karl065 <karlos065@hotmail.com>
2023-07-04 22:16:24 -0500

    Config Server

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const {conn} = require('./src/db.js');
 const {getApi} = require('./src/connectionApi/ConnectionApi');
 const PORT = 3001;
 
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.get('/createactivity', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.get('/detail', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 conn
   .sync()
   .then(async () => {
-    server.use(express.static(path.join(__dirname, 'build')));
-
-    server.get('/*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
